75f481484410e9a692ac66c783abf01291b40b55
Huy Nguyen <huynguyen260398@gmail.com>
2021-02-07 22:06:39 +0700

    Small changes for preparing to depliy to Azure

--- BookStore-UI/Static/Endpoints.cs
+++ BookStore-UI/Static/Endpoints.cs
@@ -7,7 +7,11 @@ namespace BookStore_UI.Static
 {
     public static class Endpoints
     {
+#if DEBUG
         public static string BaseUrl = "https://localhost:44360";
+#else
+        public static string BaseUrl = "";
+#endif
         public static string AuthorsEnpoint = $"{BaseUrl}/api/authors/";
         public static string BooksEnpoint = $"{BaseUrl}/api/books/";
         public static string RegisterEnpoint = $"{BaseUrl}/api/users/register/";
